293d10cb5fa0a5b691347cb88386f35fcac12a88
Artem Poboinia <artempoboinia@gmail.com>
2025-08-20 22:05:42 +0300

    Добавление функции создания задачи

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-function App() {
+import { useState } from "react";
+
+export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
   return (
-    <>
-      <h1>Hello</h1>
-      <h2>Hello</h2>
-      <h3>Hello</h3>
-    </>
+    <div>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Введи новую задачу"
+      />
+
+      <button onClick={createTask}>Создать</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
-
-export default App;
